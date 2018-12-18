c5d422a08287e2981a536cae80e10d51cd28abe0
Patrick Poole <patrick.poole@inventivhealth.com>
2018-12-18 09:24:16 -0500

    ie support

---
+++ react-doughnut-hole/precache-manifest.5f23d6eb721cdf44e64a478fac3d3d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1ed12aaf58311a10b9e",
+    "url": "/static/js/main.a1ed12aa.chunk.js"
+  },
+  {
+    "revision": "74d33e0730e4740378ee",
+    "url": "/static/js/1.74d33e07.chunk.js"
+  },
+  {
+    "revision": "a1ed12aaf58311a10b9e",
+    "url": "/static/css/main.56d50c4d.chunk.css"
+  },
+  {
+    "revision": "ce679d00f4d90d2e2e8cc7856fe7e305",
+    "url": "/index.html"
+  }
+];
